806736c1f504c2e0bc38c6846197f588bebefc06
polaris-ark3 <252160mark@gmail.com>
2025-02-10 09:53:18 +0300

    4546b

--- frontend/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    let response = await fetch("/api/anthem");
-    let data = await response.json();
-    document.getElementById("info").innerText = `Название: ${data.game}, Разработчик: ${data.developer}, Год: ${data.year}`;
-});
-
